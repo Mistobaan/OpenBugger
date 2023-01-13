91184eb1dfeb4d45185b8d9428d5d17b7ccdc959
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2023-01-13 04:35:34 -0800

    first pass

--- bin/prepare_data.mts
+++ bin/prepare_data.mts
@@ -5,3 +5,5 @@ const pblue = (s: string) => console.log(chalk.blue(s));
 
 pblue("downloading git repo ...")
 await $`git clone https://github.com/google-research-datasets/eth_py150_open.git --depth 1`
+
+await $`git clone https://github.com/google-research-datasets/eth_py150_open.git --depth 1`
---
+++ openbugger/utils/runtime.py
@@ -0,0 +1,27 @@
+import pdb
+import sys
+import traceback
+from typing import Callable
+
+__all__ = ["run_and_debug"]
+
+
+def run_and_debug(func: Callable[[], None], enable_debugging: bool) -> None:
+    """
+    A wrapper around a running script that triggers the debugger in case of an uncaught exception.
+
+    For example, this can be used as:
+    ```
+    if __name__ == '__main__':
+        run_and_debug(lambda: run(args), args['--debug'])
+    ```
+    """
+    try:
+        func()
+    except Exception:
+        if enable_debugging:
+            _, value, tb = sys.exc_info()
+            traceback.print_exc()
+            pdb.post_mortem(tb)
+        else:
+            raise
